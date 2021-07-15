1f147b237237b99f9bbd7d16b0c143ec639a23f8
Sean <sdillon215@gmail.com>
2021-07-15 16:00:02 -0700

    link to github if 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issuesContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -11,6 +12,9 @@ var getRepoIssues = function (repo) {
                     .then(function (data) {
                         // pass data to dom function
                         displayIssues(data);
+                        if (response.headers.get("Link")) {
+                            displayWarning(repo);
+                        }
                     });
             } else {
                 alert("There was a problem with your request!");
@@ -56,4 +60,17 @@ var displayIssues = function (issues) {
     }
 };
 
-getRepoIssues("sdillon215/portfolio");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "see more issues on github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
